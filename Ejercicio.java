2360675a86fb0c042d4165c0cf1f301f33410dec
chebis125 <martinezpulidojohansebastian@gmail.com>
2023-06-20 19:51:42 -0500

    new file:   Ejercicio.java

---
+++ Ejercicio.java
@@ -0,0 +1,43 @@
+public class Ejercicio {
+    public static void main(String[] args) {
+
+        Persona Opersona = new Persona();
+        Opersona.setEdad(20);
+        Opersona.setNombre("Sebastian Martinez");
+        Opersona.setTelefono(315794741);
+
+        System.out.println(Opersona.getNombre() + " tiene " + Opersona.getEdad() + " años y su numero de telefono es: " + Opersona.getTelefono());
+
+
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private long telefono;
+
+    public void setEdad (int edad){
+        this.edad = edad;
+    }
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setTelefono (long telefono){
+        this.telefono = telefono;
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public long getTelefono(){
+        return this.telefono;
+    }
+}
