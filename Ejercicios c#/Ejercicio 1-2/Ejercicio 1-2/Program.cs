8dcc8d18866e13293553cb385a52ec01ea9750d9
chebis125 <martinezpulidojohansebastian@gmail.com>
2023-06-26 16:46:03 -0500

    Nuevo ejercicio en c#

---
+++ Ejercicios c#/Ejercicio 1-2/Ejercicio 1-2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Escribe tu nombre");
+String Nombre = Console.ReadLine();
+
+Console.WriteLine("Hola, " + Nombre + "!");
+
+DateTime horaActual = DateTime.Now;
+Console.WriteLine("La hora actual es: " + horaActual.ToString("HH:mm:ss"));
