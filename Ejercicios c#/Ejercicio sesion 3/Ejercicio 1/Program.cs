4f3b7f441c69be4fa49587a1aa1b7049edb5f456
chebis125 <martinezpulidojohansebastian@gmail.com>
2023-07-04 19:44:07 -0500

    Nuevo ejercicio sesion 3

---
+++ Ejercicios c#/Ejercicio sesion 3/Ejercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Cliente cliente1 = new Cliente("Johan Sebastian Martinez Pulido",3145647,"Cra 5 sur #4-50", "martinez@gmail.com", true);
+
+Console.WriteLine(cliente1);
+
+public struct Cliente
+{
+    public Cliente(String nombre, int telefono, String direccion, String email, bool nuevo )
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        Nuevo = nuevo;
+
+    }
+    public String Nombre { get; set; }
+    public int Telefono { get; set;}
+    public String Direccion { get; set; }
+    public String Email { get; set; }
+    public bool Nuevo { get; set;}
+
+    public override string ToString() => $"{Nombre} tiene el numero {Telefono} su direccion es {Direccion}, su email {Email} y actualemente {(Nuevo ? "es un nuevo cliente" : "no es un nuevo cliente")}.";
+}
--- Ejercicios c#/Ejercicios sesion 2/Ejercicio 3/Program.cs
+++ Ejercicios c#/Ejercicios sesion 2/Ejercicio 3/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 
 bool Numero = 4 >= 18;
 bool Caracter = 'a' == 'a';
